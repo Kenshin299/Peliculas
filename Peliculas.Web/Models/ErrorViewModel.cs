8d4aa547627ad692c17994a2b874b8c3d2318a99
Roberit <roberitgonzalezt@gmail.com>
2023-02-03 15:59:24 -0400

    Primera Subida

---
+++ Peliculas.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Peliculas.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
