fba5622ce0f720b22d117bd3e88aa3858fa14896
Roberit <roberitgonzalezt@gmail.com>
2023-02-19 22:07:35 -0500

    actualizado

---
+++ Peliculas.DAL/Repositories/GeneroRepository.cs
@@ -0,0 +1,59 @@
+﻿using Generos.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using Peliculas.DAL.Context;
+using Peliculas.DAL.Entities;
+using Peliculas.DAL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Peliculas.DAL.Repositories
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        private readonly PeliculasContext context;
+        private readonly ILogger<GeneroRepository> logger;
+        public GeneroRepository(PeliculasContext context, ILogger<GeneroRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+        public void Add(Genero genero)
+        {
+            try
+            {
+                if (this.context.Generos.Any(cd => cd.CodeGenero == genero.CodeGenero))
+                {
+                    throw new PeliculasException("El genero ya se encuentra registrado");
+                }
+                this.context.Generos.Add(genero);
+                this.context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"Ha ocurrido un error: {ex.Message}", ex.ToString());
+            }
+        }
+
+        public List<Genero> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Genero GetById(int generoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Peliculas.DAL/Repositories/PeliculaRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using Peliculas.DAL.Context;
+using Peliculas.DAL.Entities;
+using Peliculas.DAL.Exceptions;
+using Peliculas.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Peliculas.DAL.Repositories
+{
+    public class PeliculaRepository : IPeliculaRepository
+    {
+        private readonly PeliculasContext context;
+        private readonly ILogger<PeliculaRepository> logger;
+        public PeliculaRepository(PeliculasContext context, ILogger<PeliculaRepository> logger)  
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+        public void Add(Pelicula pelicula)
+        {
+            try
+            {
+                if (this.context.Peliculas.Any(cd => cd.CodePelicula == pelicula.CodePelicula)) 
+                {
+                    throw new PeliculasException("La pelicula ya se encuentra registrada");
+                }
+                this.context.Peliculas.Add(pelicula);
+                this.context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"Ha ocurrido un error: {ex.Message}", ex.ToString());
+            }
+        }
+
+        public List<Pelicula> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Pelicula GetById(int peliculaId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(Pelicula pelicula)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Pelicula pelicula)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
